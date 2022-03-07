79bf7c336d19a738f3958d5dfe6d4e6e71e498f0
yuda <yudajgim@gmail.com>
2022-03-07 15:33:10 +0900

    세션, 세션셋, query properties, Statistic 수정

--- src/cafe/mvc/session/Session.java
+++ src/cafe/mvc/session/Session.java
@@ -1,23 +1,86 @@
 package cafe.mvc.session;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 로그인된 사용자의 정보를 관리하는 객체
  * 싱글톤으로 생성할 것
  * */
 public class Session {
+	private String sessionId; // 세션에서 전화번호로 사용자를 식별
+	private Map<String, Object> attributes; // 전화번호를 key로 사용자의 정보를 입력
+	
+	public Session() {}
+
+	public Session(String sessionId) {
+		super();
+		this.sessionId = sessionId;
+		attributes = new HashMap<>();
+	}
+	
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+	
+	public String getSessionId() {
+		return sessionId;
+	}
+	
+	public void setAttributes(Map<String, Object> attributes) {
+		this.attributes = attributes;
+	}
+	
+	public Object getAttributes(String name) {
+		return attributes.get(name);
+	}
+		
 	/**
-	 * 장바구니 추가
+	 * 정보 추가
+	 * @param: name - key, data - Map<k, v>로 구성된 정보(현재에는 장바구니만 사용)
 	 * */
+	public void setAttribute(String name, Object data) { // 정보를 추가함
+		attributes.put(name,data);
+	}
 	
 	/**
-	 * 장바구니 조회
+	 * 정보 조회
+	 * @param: 조회할 키 값
 	 * */
+	public Object getAttribute(String name) { // name을 가지고 data를 가져옴
+		return attributes.get(name);
+	}
 	
 	/**
-	 * 장바구니 부분 삭제
+	 * 정보 삭제
+	 * @param: 삭제할 키 값
 	 * */
+	public void removeAttribute(String name) { // name을 통해 Map 삭제
+		attributes.remove(name);
+	}
 	
 	/**
-	 * 장바구니 전체 삭제
+	 * 해시 코드 값을 리턴하는 메소드
+	 * @return: sessionId의 해시코드 값
 	 * */
+	@Override
+	public int hashCode() {
+		return sessionId.hashCode();
+	}
+	
+	/**
+	 * sessionId 중복 확인 메소드
+	 * */
+	@Override
+	public boolean equals(Object obj) {
+		Session other = (Session) obj;
+		
+		// 매개 변수로 받은 Session 객체와 현재 객체의 sessionId의 hashcode() 값을 비교함
+		if(sessionId.equals(other.sessionId)) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
 }
--- src/cafe/mvc/session/SessionSet.java
+++ src/cafe/mvc/session/SessionSet.java
@@ -1,19 +1,53 @@
 package cafe.mvc.session;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 현재 로그인한 사용자를 관리하는 Set
  * 싱글톤으로 생성할 것
  * */
 public class SessionSet {
-	/**
-	 * 로그인된 사용자 추가
-	 * */
+
+	private static SessionSet ss = new SessionSet(); // 싱글톤으로 관리하기 위해 private로 객체 생성
+	
+	private Set<Session> set;
+	private SessionSet() {
+		set = new HashSet<>();
+	}
+	
+	// public static으로 생성하고 객체 리턴해 모든 곳에서 단일 객체를 사용
+	public static SessionSet getInstance() {
+		return ss;
+	}
 	
 	/**
-	 * 이미 로그인된 사용자 중복 체크
+	 * 현재 로그인한 사용자 조회
 	 * */
+	public Session get(String sessionId) {
+		for(Session session : set) {
+			if(session.getSessionId().equals(sessionId)) {
+				return session;
+			}
+		}
+		return null;
+	}
+	
+	public Set<Session> getSet(){
+		return set;
+	}
+	
+	/**
+	 * 로그인된 사용자를 set에 추가
+	 * */
+	public void add(Session session) {
+		set.add(session);
+	}
 
 	/**
-	 * 로그아웃한 사용자 제외
+	 * 로그아웃한 사용자를 set에서 제외
 	 * */
+	public void remove(Session session) {
+		set.remove(session);
+	}
 }
