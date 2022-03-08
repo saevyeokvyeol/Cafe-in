cbbc93bb41e9dd3c8654292a7e5679f4c6b3a046
yuda <yudajgim@gmail.com>
2022-03-08 10:32:51 +0900

    스타트뷰

--- src/cafe/mvc/view/StartView.java
+++ src/cafe/mvc/view/StartView.java
@@ -3,7 +3,7 @@ package cafe.mvc.view;
 public class StartView {
 	public static void main(String[] args) {
 		System.out.println("』*，，，，，，，，，，，，，，，，，，，，，* Cafe-In拭 神重 依聖 発慎杯艦陥 *，，，，，，，，，，，，，，，，，，，，，*』");
-		MenuView2.mainMenu();
+		MenuView.menu();
 	}
 
 }
