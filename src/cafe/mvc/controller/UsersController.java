fe4ce55abb65fcbc71f6fd8387d4660ac267fd07
woojin <jungwj92@gmail.com>
2022-03-09 14:04:05 +0900

    메뉴뷰 2 제거

--- src/cafe/mvc/controller/UsersController.java
+++ src/cafe/mvc/controller/UsersController.java
@@ -4,7 +4,7 @@ import cafe.mvc.model.dto.Users;
 import cafe.mvc.model.service.UsersService;
 import cafe.mvc.model.service.UsersServiceImpl;
 import cafe.mvc.view.FailView;
-import cafe.mvc.view.MenuView2;
+import cafe.mvc.view.MenuView;
 import cafe.mvc.view.SuccessView;
 
 
@@ -17,12 +17,7 @@ public class UsersController {
 	public static void login(String userTel, int userPwd) {
 		try {
 			Users users = usersService.login(userTel, userPwd);
-//			MenuView.printUserMenu(users.getUserTel(), users.getUserName());
-			if(users.getUserTel().equals("999-9999-9999")) {
-				MenuView2.adminMenu(users.getUserTel(), users.getUserName());
-			} else {
-				MenuView2.userMenu(users.getUserTel(), users.getUserName());
-			}
+			MenuView.printUserMenu(users.getUserTel(), users.getUserName());
 		} catch (Exception e) {
 			FailView.errorMessage(e.getMessage());
 
@@ -79,7 +74,7 @@ public class UsersController {
 	public static void userCouponCh(Users users) {
 		try {
 			usersService.userPointCh(users);
-			SuccessView.printMessage("적립금확인완료");
+			SuccessView.printMessage("쿠폰확인완료");
 			//MenuView.menu();
 		}catch (Exception e) {
 			e.printStackTrace();
